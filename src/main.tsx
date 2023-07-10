d5c661d64a54761458b02d29a86c6c9db1ea7d6a
Ezaz Ahmed <ezaznahmed@gmail.com>
2023-07-10 14:41:11 +0600

    :heavy_plus_sign: wouter introduce

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Route } from 'wouter';
+
+import Entry from '~/routers/Entry';
+import './index.css';
+
+const Router = () => {
+  return (
+    <>
+      <Route path='/' component={Entry} />
+    </>
+  );
+};
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Router />
+  </React.StrictMode>
+);
