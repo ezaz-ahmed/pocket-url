69858fa3053037763058ad5ee91de8d6ce6f94ef
Ezaz Ahmed <ezaznahmed@gmail.com>
2023-07-10 13:39:42 +0600

    :tada: Initial commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Route } from 'wouter';
-
-import './index.css';
-import Entry from './routes/Entry.tsx';
-
-const Router = () => {
-  return <Route path='/' component={Entry} />;
-};
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Router />
-  </React.StrictMode>
-);
+    <App />
+  </React.StrictMode>,
+)
--- src/routes/Entry.tsx
+++
@@ -1,5 +0,0 @@
-const Entry = () => {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
-};
-
-export default Entry;
