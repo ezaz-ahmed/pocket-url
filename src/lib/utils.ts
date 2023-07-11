a06b300670c9eca84f6ff2d38643032ebee3762e
Ezaz Ahmed <ezaznahmed@gmail.com>
2023-07-11 13:46:26 +0600

    :sparkles: Input components

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,11 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
- 
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function linkValidation(link: string) {
+  const urlRegex = /^(https?:\/\/)?([\w.-]+)\.([a-z]{2,})(\/\S*)?$/
+  return urlRegex.test(link)
 }
--- src/pages/Entry.tsx
+++ src/pages/Entry.tsx
@@ -1,13 +1,33 @@
+import { useState } from 'react';
+import { Button } from '~/components/ui/Button';
+
+import { Input } from '~/components/ui/Input';
+
 const Entry = () => {
+  const [link, setLink] = useState<string>('');
+
   return (
-    <div className='mx-auto mb-10 mt-12 max-w-md px-2.5 text-center sm:max-w-lg sm:px-0'>
-      <h1 className='mt-5 font-display text-4xl font-extrabold leading-[1.15] text-black sm:text-6xl sm:leading-[1.15]'>
-        Short Links With
-        <br />
-        <span className='bg-gradient-to-r from-amber-500 via-orange-600 to-yellow-500 bg-clip-text text-transparent'>
-          Pocket URL
-        </span>
-      </h1>
+    <div className=' flex flex-col items-center'>
+      <div className='mx-auto mb-10 mt-12 max-w-md px-2.5 text-center sm:max-w-lg sm:px-0'>
+        <h1 className='mt-5 font-display text-4xl font-extrabold leading-[1.15] text-black sm:text-6xl sm:leading-[1.15]'>
+          Short Links With
+          <br />
+          <span className='bg-gradient-to-r from-amber-500 via-orange-600 to-yellow-500 bg-clip-text text-transparent'>
+            Pocket URL
+          </span>
+        </h1>
+      </div>
+
+      <form className='flex w-full max-w-sm items-center space-x-2'>
+        <Input
+          type='text'
+          required
+          value={link}
+          onChange={event => setLink(event.target.value)}
+          placeholder='Enter your long URL here'
+        />
+        <Button type='submit'>Shorten</Button>
+      </form>
     </div>
   );
 };
