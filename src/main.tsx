23616815985c58496e9ffe9716bb803e75bf4d08
Ezaz Ahmed <ezaznahmed@gmail.com>
2023-07-11 12:46:50 +0600

    :bug: warnings and bug fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Route } from 'wouter';
+import { Route, Router } from 'wouter';
 
-import Entry from '~/routers/Entry';
+import Entry from '~/pages/Entry';
 import { Navbar } from '~/components/layout/Navbar';
 import './index.css';
 
-const Router = () => {
+const App = () => {
   return (
-    <>
+    <Router>
       <Navbar />
       <Route path='/' component={Entry} />
-    </>
+    </Router>
   );
 };
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Router />
+    <App />
   </React.StrictMode>
 );
