07ec78f717264f4bee6c0cdcc0005899298b0326
Ezaz Ahmed <ezaznahmed@gmail.com>
2023-07-16 13:01:09 +0600

    :sparkles: shows generated link

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,11 +1,11 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
-export function cn(...inputs: ClassValue[]) {
+export function cn(...inputs: ClassValue[]): string {
   return twMerge(clsx(inputs))
 }
 
-export function isUrlValid(url: string) {
-  const pattern = /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i;
-  return !!pattern.test(url);
-};
+export function isUrlValid(url: string): boolean {
+  const urlRegex = new RegExp("^(https?://)?([a-zA-Z0-9.-]+)(:[0-9]+)?(/[a-zA-Z0-9%_.~:/?#[\\]@!$&'()*+,;=-]*)?$")
+  return urlRegex.test(url)
+}
--- src/pages/Entry.tsx
+++ src/pages/Entry.tsx
@@ -1,77 +1,75 @@
-import { RefreshCcw } from 'lucide-react';
 import { FormEvent, useState, useRef, ChangeEvent, useEffect } from 'react';
+import { FilesIcon, MoveRight, RefreshCcw } from 'lucide-react';
 
 import { Button } from '~/components/ui/Button';
 import { Input } from '~/components/ui/Input';
 import { ShortenedUrl, ShortenedUrlResponse } from '~/lib/types';
 import { isUrlValid } from '~/lib/utils';
+import { Card } from '~/components/ui/Card';
+import { Link } from 'wouter';
 
 const Entry = () => {
   const linkInput = useRef<HTMLInputElement>(null);
   const [shortenedUrls, setShortenedUrls] = useState<ShortenedUrl[]>([]);
   const [link, setLink] = useState<string>('');
-  const [loading, setLoading] = useState<boolean>(false)
-  const [error, setError] = useState<string>("")
-
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string>('');
 
   useEffect(() => {
-    linkInput.current?.focus()
+    linkInput.current?.focus();
 
     const savedUrls = localStorage.getItem('shortenedUrls');
     if (savedUrls) {
       setShortenedUrls(JSON.parse(savedUrls));
     }
-  }, [])
+  }, []);
 
+  const latestUrl = shortenedUrls[shortenedUrls.length - 1];
 
   const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
+    event.preventDefault();
     if (!linkInput.current) return;
 
     if (isUrlValid(link)) {
-
       try {
-        setLoading(true)
+        setLoading(true);
         const response = await fetch(
           `https://api.shrtco.de/v2/shorten?url=${link}`
-        )
-        const data = await response.json() as ShortenedUrlResponse
+        );
+        const data = (await response.json()) as ShortenedUrlResponse;
 
-        setLoading(false)
-        setLink("")
+        setLoading(false);
+        setLink('');
 
         if (data.ok) {
           const { result } = data;
           const updatedUrls = [...shortenedUrls, result];
 
-
           setShortenedUrls(updatedUrls);
 
-          localStorage.setItem('shortenedUrls', JSON.stringify(updatedUrls))
+          localStorage.setItem('shortenedUrls', JSON.stringify(updatedUrls));
         } else {
-          throw new Error("Something went wrong!")
+          throw new Error('Something went wrong!');
         }
-
-
       } catch (error: any) {
-        setLoading(false)
-        setLink("")
+        setLoading(false);
+        setLink('');
 
         if (error.message) {
-          setError(error.message)
+          setError(error.message);
         } else {
-          setError("Something went wrong!")
+          setError('Something went wrong!');
         }
       }
     } else {
-      linkInput.current.setCustomValidity("Please enter a valid URL")
+      linkInput.current.setCustomValidity('Please enter a valid URL');
     }
-  }
+  };
 
   const handleInput = (event: ChangeEvent<HTMLInputElement>) => {
-    linkInput.current?.setCustomValidity("")
-    setLink(event.target.value)
-  }
+    linkInput.current?.setCustomValidity('');
+    setLink(event.target.value);
+  };
 
   return (
     <div className=' flex flex-col items-center'>
@@ -85,7 +83,10 @@ const Entry = () => {
         </h1>
       </div>
 
-      <form className='flex w-full max-w-sm items-center justify-between space-x-2' onSubmit={handleSubmit}>
+      <form
+        className='flex w-full max-w-sm items-center justify-between space-x-2'
+        onSubmit={handleSubmit}
+      >
         <Input
           type='text'
           ref={linkInput}
@@ -96,17 +97,50 @@ const Entry = () => {
         />
 
         <Button disabled={loading} className=' w-28'>
-          {loading ?
-
-            <RefreshCcw className="mr-3 h-5 w-5 animate-spin text-white dark:dark:text-gray-900" />
-
-            :
-            <span className="font-medium">Shorten</span>
-          }
+          {loading ? (
+            <RefreshCcw className='mr-3 h-5 w-5 animate-spin text-white dark:dark:text-gray-900' />
+          ) : (
+            <span className='font-medium'>Shorten</span>
+          )}
         </Button>
       </form>
 
       {error && <p>{error}</p>}
+
+      {latestUrl && (
+        <>
+          <Card className=' w-full max-w-sm mt-4 py-3 px-5 flex justify-between items-center'>
+            <div>
+              <a
+                target='_blank'
+                href={`https://${latestUrl.short_link}`}
+                className='font-medium text-blue-600 underline dark:text-blue-500 hover:no-underline text-xl'
+              >
+                {latestUrl.short_link}
+              </a>
+              <p className=' text-base line-clamp-1 max-w-[220px]'>
+                {latestUrl.original_link}
+              </p>
+            </div>
+
+            <Button
+              variant='outline'
+              size='icon'
+              onClick={() =>
+                navigator.clipboard.writeText(latestUrl.short_link)
+              }
+            >
+              <FilesIcon className='h-4 w-4' />
+            </Button>
+          </Card>
+
+          <Link href='/list'>
+            <Button className=' my-4'>
+              See More <MoveRight className=' h-4 w-4 mx-3' />
+            </Button>
+          </Link>
+        </>
+      )}
     </div>
   );
 };
